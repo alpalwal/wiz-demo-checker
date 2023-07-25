2e032d7a2f9919591a8c36921c71ac1f57f402cf
Alex Corstorphine <alexcorstorphine@Alexs-MacBook-Pro.local>
2023-07-25 09:06:18 -0600

    adding subscribers

--- main.tf
+++ main.tf
@@ -76,15 +76,29 @@ resource "aws_sns_topic_subscription" "demo_issue_topic_subscription1" {
   protocol  = "email"
   endpoint  = "alex@wiz.io"
 }
+
 resource "aws_sns_topic_subscription" "demo_issue_topic_subscription2" {
   topic_arn = resource.aws_sns_topic.demo_issue_topic.arn
   protocol  = "email"
-  endpoint  = "jeff.morgan@wiz.io"
+  endpoint  = "jonathan.dong@wiz.io"
 }
+
 resource "aws_sns_topic_subscription" "demo_issue_topic_subscription3" {
   topic_arn = resource.aws_sns_topic.demo_issue_topic.arn
   protocol  = "email"
+  endpoint  = "avi.konstantini@wiz.io"
+}
+
+resource "aws_sns_topic_subscription" "demo_issue_topic_subscription4" {
+  topic_arn = resource.aws_sns_topic.demo_issue_topic.arn
+  protocol  = "email"
   endpoint  = "bryan.mcclellan@wiz.io"
+}
+
+resource "aws_sns_topic_subscription" "demo_issue_topic_subscription5" {
+  topic_arn = resource.aws_sns_topic.demo_issue_topic.arn
+  protocol  = "email"
+  endpoint  = "jeff.morgan@wiz.io"
 }
 
 resource "aws_cloudwatch_event_rule" "demo_checker_lambda_event_rule" {
